5823f470016a5e0f21d03b025ca87f053379c73f
whats0n <mrdvv@mail.ru>
2015-01-26 17:54:10 +0200

    done

--- js/common.js
+++ js/common.js
@@ -15,5 +15,75 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	console.log($('body').html());
+	$('.nav-btn').on('click', function() {
+		if ($(this).hasClass('is-active')) {
+			$('.nav-mob').removeClass('is-open');
+			$(this).removeClass('is-active');
+		}
+		else {
+			$('.nav-mob').addClass('is-open');
+			$(this).addClass('is-active');
+		};
+		return false;
+	});
+	//scroll fixed
+	$(window).scroll(function () {
+		var $this = $(this),
+			$head = $('.body');
+			if ($this.scrollTop() > $('.content').offset().top) {
+				$head.addClass('is-fixed');
+			} else {
+				$head.removeClass('is-fixed');
+			};
+	});	
+	if ($(document).scrollTop() > $('.content').offset().top) {
+        $(".body").addClass("is-fixed");	
+	}
+	else {
+        $(".body").removeClass("is-fixed");
+	};
+	//popup
+	$('.js-form').on('click', function() {
+		if ($(this).hasClass('is-active')) {
+			$(this).removeClass('is-active');
+			$('.js-popup').removeClass('is-active');
+			$('body').removeClass('no-scroll');
+		}
+		else {
+			$(this).addClass('is-active');
+			$('.js-popup').addClass('is-active');	
+			$('body').addClass('no-scroll');		
+		};
+		return false;
+	});
+	//tabs
+	$('.form__tabs a').click(function(event) {
+		/* Act on the event */
+		$('.form__tabs a').removeClass('is-active');
+		$(this).addClass('is-active');
+		attr = $(this).attr('href');
+		$('#is-in,#is-out').addClass('is-hidden');
+		$(' '+attr).removeClass('is-hidden');
+		return false;
+	});
+	//form
+	$('.form__bottom a').click(function(event) {
+		/* Act on the event */
+		$('.form__bottom a').removeClass('is-active');
+		$(this).addClass('is-active');
+		attr = $(this).attr('href');
+		$('#form1,#form2,#form3,#form4,#form5,#form6,#form7').addClass('is-hidden');
+		$(' '+attr).removeClass('is-hidden');
+		return false;
+	});
+	$('.js-is-in').on('click', function() {
+		$('.form__block').addClass('is-hidden');
+		$('.form-1').removeClass('is-hidden');
+		return false;		
+	});
+	$('.js-is-out').on('click', function() {
+		$('.form__block').addClass('is-hidden');
+		$('.form-4').removeClass('is-hidden');
+		return false;		
+	});
 });

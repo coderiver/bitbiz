7bdda3feafecce91d0ae9bd733f3467e8df9c781
whats0n <mrdvv@mail.ru>
2015-03-04 19:13:15 +0200

    done/fixed

--- js/common.js
+++ js/common.js
@@ -96,8 +96,15 @@ head.ready(function() {
 	//tabs
 	$('.form__tabs a').click(function(event) {
 		/* Act on the event */
-		$('.form__tabs a').removeClass('is-active');
-		$(this).addClass('is-active');
+		if ($(this).hasClass('js-is-in')) {
+			$('.js-is-out').removeClass('is-active');
+			$('.js-is-in').addClass('is-active');
+		}
+		else {
+
+			$('.js-is-out').addClass('is-active');
+			$('.js-is-in').removeClass('is-active');
+		};		
 		attr = $(this).attr('href');
 		$('#is-in,#is-out').addClass('is-hidden');
 		$(' '+attr).removeClass('is-hidden');
@@ -123,4 +130,117 @@ head.ready(function() {
 		$('.form-4').removeClass('is-hidden');
 		return false;		
 	});
+	//circle
+	var circles = $('.circles');
+
+	if ( circles.length ) {
+
+	    var circlesMain     = circles.find('.circles__items'),
+	        circlesItem     = circles.find('.circles__item'),
+	        centerBtn       = circles.find('.circles__center'),
+	        circlesItems    = circlesItem.length,
+	        step            = (360 / circlesItems),
+	        activeClass     = 'is-active',
+	        angle           = 0,
+	        activeItemIndex = 0;
+
+	    var rotateCircles = function(angle) {
+	        circlesMain.css({
+	            '-webkit-transform' : 'translateZ(0) rotate(' + angle + 'deg)',
+	                '-ms-transform' : 'translateZ(0) rotate(' + angle + 'deg)',
+	                    'transform' : 'translateZ(0) rotate(' + angle + 'deg)'
+	        });
+
+	        circlesItem.css({
+	           '-webkit-transform' : 'translateZ(0) rotate(' + -angle + 'deg)',
+	               '-ms-transform' : 'translateZ(0) rotate(' + -angle + 'deg)',
+	                   'transform' : 'translateZ(0) rotate(' + -angle + 'deg)'
+	        });
+	    };
+
+	    circlesItem.each(function(index, el) {
+
+	        $(el).on('click', function(event) {
+	            event.preventDefault();
+	          	// $(circlesItem[activeItemIndex]).removeClass(activeClass);
+	          	// $(el).addClass(activeClass);
+
+	            angle           = step * index;
+	            activeItemIndex = index;
+
+	            rotateCircles(angle);
+	        });
+
+	    });
+
+	    centerBtn.on('click', function(event) {
+	        event.preventDefault();
+
+	        var itemToClick;
+
+	        if ( activeItemIndex < circlesItems - 1 ) {
+	            itemToClick = activeItemIndex + 1;
+	        } else {
+	            itemToClick = 0;
+	        }
+
+	        $(circlesItem[itemToClick]).trigger('click');
+
+	    });
+
+	};
+	var tabsFunc = function() {
+
+	    var container = $('.js-tab-container');
+
+	    if ( container.length ) {
+	        container.each(function() {
+	            var el           = $(this),
+	                tab          = el.find('.js-tab'),
+	                content      = el.find('.js-tab-content'),
+	                activeClass  = 'is-active',
+	                tabs         = [],
+	                tabsContent  = [],
+	                activeTab;
+	            //create associative array with tabs content as jQuery object
+	            content.each(function() {
+	                var tabNumber = $(this).data('tab');
+	                tabsContent[tabNumber] = $(this);
+	            });
+
+	            tab.each(function() {
+	                var el        = $(this),
+	                    tabNumber = el.data('tab');
+
+	                //create associative array with tabs as jQuery object
+	                tabs[tabNumber] = el;
+
+	                if ( el.hasClass(activeClass) ) {
+	                    activeTab = tabNumber;
+	                }
+
+	                el.on('click', function(event) {
+	                    event.preventDefault();
+
+	                    tabs[activeTab].removeClass(activeClass);
+	                    tabsContent[activeTab].removeClass(activeClass);
+
+	                    el.addClass(activeClass);
+	                    tabsContent[tabNumber].addClass(activeClass);
+
+	                    activeTab = tabNumber;
+	                });
+	            });
+
+	            //if active tab is not defined then make first tab as active
+	            if ( !activeTab ) {
+	                activeTab = 1;
+	                tabs[activeTab].addClass(activeClass);
+	                tabsContent[activeTab].addClass(activeClass);
+	            }
+	        });
+	    }
+
+	};
+	tabsFunc();
 });
